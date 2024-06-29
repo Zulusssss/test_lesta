ef1ff71cd8335e05c2d286ee5cd7f25b2d24f71c
Rostyslav Zhytkov <100017508+Zulusssss@users.noreply.github.com>
2024-06-29 18:19:38 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -12,25 +12,20 @@ def isEven_new(value):
 
 arr = list(range(-100, 1, 100))
 
-# Решение isEven
 sol1 = """
 result = [isEven(x) for x in arr]
 """
 
-# Решение isEven_new
 sol2 = """
 result = [isEven_new(x) for x in arr]
 """
 
-# Подготовка для timeit
 setup = """
 from __main__ import isEven, isEven_new, arr
 """
 
-# Измерение времени выполнения
 time1 = timeit.timeit(sol1, setup=setup, number=1)
 time2 = timeit.timeit(sol2, setup=setup, number=1)
-
 
 print(f"Время выполнения решения №1: {time1} секунд")
 print(f"Время выполнения решения №2: {time2} секунд")
